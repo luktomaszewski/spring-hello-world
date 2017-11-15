43c681742581759d0d999713e305ece449aa8a9a
Łukasz Tomaszewski <lukasz.tomaszewski89@gmail.com>
2017-11-15 14:03:41 +0100

    Fixed issue with Repository Injection in REST Controller

--- src/main/java/xyz/lomasz/springhelloworld/dao/AirlineRepository.java
+++ src/main/java/xyz/lomasz/springhelloworld/dao/AirlineRepository.java
@@ -1,13 +1,13 @@
 package xyz.lomasz.springhelloworld.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import xyz.lomasz.springhelloworld.model.Airline;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource
+@Repository
 public interface AirlineRepository extends CrudRepository<Airline, Long> {
     List<Airline> findAll();
 
