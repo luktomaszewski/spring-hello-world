36dd96b4f966b1bab22e57729f99fa357b0eabec
Łukasz Tomaszewski <lukasz.tomaszewski89@gmail.com>
2017-11-15 10:52:22 +0100

    Migration from Maven to Gradle
    Migration from Maven to Gradle

--- src/test/java/xyz/lomasz/springhelloworld/controller/AirlineControllerTest.java
+++ src/test/java/xyz/lomasz/springhelloworld/controller/AirlineControllerTest.java
@@ -1,10 +1,10 @@
 package xyz.lomasz.springhelloworld.controller;
 
-import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.filter.log.RequestLoggingFilter;
-import com.jayway.restassured.filter.log.ResponseLoggingFilter;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.specification.RequestSpecification;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +15,7 @@ import xyz.lomasz.springhelloworld.model.CrewType;
 
 import java.util.Arrays;
 
-import static com.jayway.restassured.RestAssured.given;
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
