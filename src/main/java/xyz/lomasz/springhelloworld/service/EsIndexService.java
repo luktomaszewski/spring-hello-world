e43d398fd199275f51114ec798e6fe6b3703afbf
lomasz <lukasz.tomaszewski89@gmail.com>
2018-01-08 09:03:32 +0100

    Created unit tests headers for Elasticsearch index service

--- src/main/java/xyz/lomasz/springhelloworld/service/EsIndexService.java
+++ src/main/java/xyz/lomasz/springhelloworld/service/EsIndexService.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import lombok.extern.apachecommons.CommonsLog;
 import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xyz.lomasz.springhelloworld.model.Airline;
@@ -24,18 +27,22 @@ public class EsIndexService {
     this.indexName = indexName;
   }
 
-  public void index(Airline airline) throws IOException {
+  public RestStatus index(Airline airline) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     String jsonString = mapper.writeValueAsString(airline);
 
     IndexRequest indexRequest = new IndexRequest(indexName, indexName, airline.getIcao());
     indexRequest = indexRequest.source(jsonString, XContentType.JSON);
-    client.index(indexRequest);
+    IndexResponse indexResponse = client.index(indexRequest);
+
+    return indexResponse.status();
   }
 
-  public void delete(Airline airline) throws IOException {
+  public RestStatus deleteIndex(Airline airline) throws IOException {
     DeleteRequest deleteRequest = new DeleteRequest(indexName, indexName, airline.getIcao());
-    client.delete(deleteRequest);
+    DeleteResponse deleteResponse = client.delete(deleteRequest);
+
+    return deleteResponse.status();
   }
 
 }
---
+++ src/test/java/xyz/lomasz/springhelloworld/service/EsIndexServiceTest.java
@@ -0,0 +1,82 @@
+package xyz.lomasz.springhelloworld.service;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class EsIndexServiceTest {
+
+  @Test
+  public void shouldIndexAirlineReturnsCreatedStatus() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+  @Test
+  public void shouldIndexAirlineReturnsUpdatedStatus() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+  @Test
+  public void shouldIndexAirlineReturnsIOException() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+  @Test
+  public void shouldDeleteIndexAirlineReturnsNotFoundStatus() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+
+  @Test
+  public void shouldDeleteIndexAirlineReturnsDeletedStatus() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+  @Test
+  public void shouldDeleteIndexAirlineReturnsIOException() throws IOException {
+    // given
+
+
+    // when
+
+
+    // then
+
+  }
+
+
+}
