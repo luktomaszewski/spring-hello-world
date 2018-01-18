de43087992ee8b9af62498d253660d87d4f8d371
lomasz <lukasz.tomaszewski89@gmail.com>
2018-01-18 17:46:25 +0100

    Removed wrong unit tests

--- src/test/java/xyz/lomasz/springhelloworld/controller/AirlineControllerTest.java
+++ src/test/java/xyz/lomasz/springhelloworld/controller/AirlineControllerTest.java
@@ -1,55 +1,13 @@
 package xyz.lomasz.springhelloworld.controller;
 
-import static io.restassured.RestAssured.given;
-
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import java.util.Arrays;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import xyz.lomasz.springhelloworld.model.Airline;
-import xyz.lomasz.springhelloworld.model.Airplane;
-import xyz.lomasz.springhelloworld.model.Crew;
-import xyz.lomasz.springhelloworld.model.CrewType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 public class AirlineControllerTest {
-
-  private static final String PATH = "airline";
-  private static RequestSpecification spec;
-
-  @LocalServerPort
-  int randomServerPort;
-
-  @Before
-  public void initSpec() {
-    spec = new RequestSpecBuilder()
-        .setContentType(ContentType.JSON)
-        .setPort(randomServerPort)
-        .addFilter(new ResponseLoggingFilter())
-        .addFilter(new RequestLoggingFilter())
-        .build();
-  }
-
-  private Airline createDummyAirline(String name) {
-    Airline dummyAirline = new Airline();
-
-    dummyAirline.setIcao(name);
-    dummyAirline.setName(name + " Airline");
-    dummyAirline.setHub(name + " Airport");
-    dummyAirline.setFleet(Arrays.asList(new Airplane(name, "B788")));
-    dummyAirline.setCrew(Arrays.asList(new Crew(name, "Pilot", CrewType.PILOT)));
-
-    return dummyAirline;
-  }
 
   @Test
   public void createAirlineNewShouldReturnCreatedStatus() throws Exception {
